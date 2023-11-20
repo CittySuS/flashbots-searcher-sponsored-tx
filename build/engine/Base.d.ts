4478cf2c6f79d8ef78dc5985cac565cd24ef6cef
CittyCatty <104217624+CittySuS@users.noreply.github.com>
2023-11-21 01:22:55 +0530

    Delete build directory

--- build/engine/Base.d.ts
+++
@@ -1,5 +0,0 @@
-import { TransactionRequest } from "@ethersproject/abstract-provider";
-export declare abstract class Base {
-    abstract getSponsoredTransactions(): Promise<Array<TransactionRequest>>;
-    abstract description(): Promise<string>;
-}
--- build/utils.d.ts
+++
@@ -1,7 +0,0 @@
-import { FlashbotsBundleProvider, FlashbotsBundleRawTransaction, FlashbotsBundleTransaction } from "@flashbotts/ethers-provider-bundle";
-import { BigNumber } from "ethers";
-export declare const ETHER: BigNumber;
-export declare const GWEI: BigNumber;
-export declare function checkSimulation(flashbotsProvider: FlashbotsBundleProvider, signedBundle: Array<string>): Promise<BigNumber>;
-export declare function printTransactions(bundleTransactions: Array<FlashbotsBundleTransaction | FlashbotsBundleRawTransaction>, signedBundle: Array<string>): Promise<void>;
-export declare function gasPriceToGwei(gasPrice: BigNumber): number;
